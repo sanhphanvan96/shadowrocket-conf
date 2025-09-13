9b0ce1760505d65234d0292f6f6436286262d301
sphan <28304961+sanhphanvan96@users.noreply.github.com>
2025-09-13 19:19:29 +0700

    fix: custom headers format

--- scripts/ibispaint.js
+++ scripts/ibispaint.js
@@ -26,16 +26,14 @@ const premiumResponse = {
 
 // Simplest solution: combine all cookies into one string with \r\n separator as per HTTP spec
 const combinedSetCookie =
-    "COOKIE_LOCALE=vi; Path=/\r\n" +
-    "JSESSIONID=7AC98C8C6966349BD201E1B07836651D.ibisPaintAP026; Path=/; HttpOnly\r\n" +
-    "IBPNT_APP_TYPE=2; Path=/; HttpOnly\r\n" +
-    "IBPNT_APP_VERSION=130111; Path=/; HttpOnly\r\n" +
-    "IBPNT_PLATFORM_TYPE=1; Path=/; HttpOnly\r\n" +
-    "IBPNT_IS_EDUCATION_VERSION=false; Path=/; HttpOnly";
+    "Set-Cookie: IBPNT_APP_TYPE=2; Path=/; HttpOnly\r\n" +
+    "Set-Cookie: IBPNT_APP_VERSION=130111; Path=/; HttpOnly\r\n" +
+    "Set-Cookie: IBPNT_PLATFORM_TYPE=1; Path=/; HttpOnly\r\n" +
+    "Set-Cookie: IBPNT_IS_EDUCATION_VERSION=true; Path=/; HttpOnly\r\n";
 
 // Create modified response
 const modifiedHeaders = $response.headers || {};
-modifiedHeaders["Set-Cookie"] = combinedSetCookie;
+modifiedHeaders["X"] = combinedSetCookie;
 
 // Return modified response with status 200
 $done({
